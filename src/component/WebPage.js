78ae81a50e0b2c7a83526de009a31ab1309c7a30
Krizhanovssskiy <Krizhanovssskiy@gmail.com>
2018-12-18 16:47:20 +0200

    React_Like_Dislike

---
+++ src/component/WebPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WebPage =() => (
+	<div>
+
+	</div>
+);
+
+export default WebPage;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import App from './App';
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'ADD_LIKE':
+			return Object.assign({}, state, { likes: state.likes + 1 } );
+		case 'ADD_DISLIKE':
+			return Object.assign({}, state, { dislikes: state.dislikes + 1 } );
+		default:
+			return state;
+	}
+};
+const initialState = {
+	views: 0,
+	likes: 0,
+	dislikes: 0,
+};
+
+const store = createStore(reducer, initialState);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
+
